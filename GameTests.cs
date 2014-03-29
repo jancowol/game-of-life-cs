15afb6f877d9834aaab936bb34ecf38f03651430
Janco Wolmarans <jancow@gmail.com>
2014-03-29 21:11:06 +0200

    Add test for one live cell with two live neighbours should live on

--- GameTests.cs
+++ GameTests.cs
@@ -25,5 +25,18 @@ namespace gol
 
 			Assert.That(evolvedUniverse.LiveCells, Is.Empty);
 		}
+
+		[Test]
+		public void GivenAUniverseWithALiveCellWithTwoLiveNeighbours_WhenEvolved_TheCellIsStillAlive()
+		{
+			var liveCell = new Cell();
+			var liveCellNeighbour1 = new Cell();
+			var liveCellNeighbour2 = new Cell();
+			var universeWithLiveCells = new Universe(liveCell, liveCellNeighbour1, liveCellNeighbour2);
+			var game = new Game();
+			var evolvedUniverse = game.Evolve(universeWithLiveCells);
+
+			Assert.That(evolvedUniverse.LiveCells, Contains.Item(liveCell));
+		}
 	}
 }
