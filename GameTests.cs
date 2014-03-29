0dec936507891aa13ddd18ea5d4ca5d6f287ff7e
Janco Wolmarans <jancow@gmail.com>
2014-03-29 18:55:11 +0200

    Universe with no living cells remains devoid of living cells

---
+++ GameTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class GameTests
+{
+	[Test]
+	public void GivenAUniverseWithNoLivingCells_WhenEvolved_TheUniverseContainsNoLivingCells()
+	{
+		var emptyUniverse = new Universe();
+		var game = new Game();
+		var evolvedUniverse = game.Evolve(emptyUniverse);
+
+		Assert.That(evolvedUniverse.LiveCells, Is.Empty);
+	}
+}
+
+public class Game
+{
+	public Universe Evolve(Universe universe)
+	{
+		return new Universe();
+	}
+}
+
+public class Universe
+{
+	public object[] LiveCells { get; set; }
+
+	public Universe()
+	{
+		LiveCells = new object[0];
+	}
+}
