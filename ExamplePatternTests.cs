350142ebce587b75c29dbd996c86c3fcfd5ebe01
Janco Wolmarans <jancow@gmail.com>
2014-03-30 22:10:38 +0200

    Convert Blinker test to generic pattern based test

--- ExamplePatternTests.cs
+++ ExamplePatternTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 
 namespace gol
@@ -10,16 +9,13 @@ namespace gol
 		[Test]
 		public void Blinker()
 		{
-			var evolvedUniverse = Evolve(CellsFor(
+			Cells(
 				"   ",
-				"XXX"));
-
-			var expected = CellsFor(
+				"XXX")
+			.TransformsTo(
 				" X ",
 				" X ",
 				" X ");
-
-			Assert.That(evolvedUniverse.LiveCells, Is.EquivalentTo(expected));
 		}
 
 		[Test]
@@ -27,31 +23,14 @@ namespace gol
 		{
 			var block = new[] { new Cell(1, 1), new Cell(2, 1), new Cell(1, 2), new Cell(2, 2) };
 
-			var evolvedUniverse = Evolve(block);
+			var evolvedUniverse = PatternTestExtensions.Evolve(block);
 
 			Assert.That(evolvedUniverse.LiveCells, Is.EquivalentTo(new[] {new Cell(1, 1), new Cell(2, 1), new Cell(1, 2), new Cell(2, 2)}));
 		}
 
-		private static IEnumerable<ICell> CellsFor(params string[] pattern)
+		private static IEnumerable<ICell> Cells(params string[] pattern)
 		{
-			for (int y = 0; y < pattern.Length; y++)
-			{
-				var line = pattern[y];
-				for (int x = 0; x < line.Length; x++)
-				{
-					if (line[x] == 'X')
-					{
-						yield return new Cell(x, y);
-					}
-				}
-			}
-		}
-
-		private static IUniverse Evolve(IEnumerable<ICell> threeInARow)
-		{
-			var game = new Game();
-			var initialUniverse = new Universe(threeInARow.ToArray());
-			return game.Evolve(initialUniverse);
+			return PatternTestExtensions.Cells(pattern);
 		}
 	}
 }
---
+++ PatternTestExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace gol
+{
+	public static class PatternTestExtensions
+	{
+		public static void TransformsTo(this IEnumerable<ICell> initial, params string[] expectedPattern)
+		{
+			var evolvedUniverse = Evolve(initial);
+			var expected = Cells(expectedPattern);
+			Assert.That(evolvedUniverse.LiveCells, Is.EquivalentTo(expected));
+		}
+
+		public static IUniverse Evolve(IEnumerable<ICell> threeInARow)
+		{
+			var game = new Game();
+			var initialUniverse = new Universe(threeInARow.ToArray());
+			return game.Evolve(initialUniverse);
+		}
+
+		public static IEnumerable<ICell> Cells(params string[] pattern)
+		{
+			for (int y = 0; y < pattern.Length; y++)
+			{
+				var line = pattern[y];
+				for (int x = 0; x < line.Length; x++)
+					if (line[x] == 'X')
+						yield return new Cell(x, y);
+			}
+		}
+	}
+}
