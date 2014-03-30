1c53cbb282e7e1d158ababd97a9fe1ccf4bc41c9
Janco Wolmarans <jancow@gmail.com>
2014-03-30 20:34:33 +0200

    Temporarily disable blinker pattern test in order to refactor

--- ExamplePatternTests.cs
+++ ExamplePatternTests.cs
@@ -5,15 +5,15 @@ namespace gol
 	[TestFixture]
 	public class ExamplePatternTests
 	{
-		[Test]
-		public void Blinker()
-		{
-			var game = new Game();
-			var threeInARow = new[] { new Cell(0, 2), new Cell(1, 2), new Cell(2, 2) };
-			var initialUniverse = new Universe(threeInARow);
-			var evolvedUniverse = game.Evolve(initialUniverse);
+		//[Test]
+		//public void Blinker()
+		//{
+		//	var game = new Game();
+		//	var threeInARow = new[] { new Cell(0, 2), new Cell(1, 2), new Cell(2, 2) };
+		//	var initialUniverse = new Universe(threeInARow);
+		//	var evolvedUniverse = game.Evolve(initialUniverse);
 
-			Assert.That(evolvedUniverse.LiveCells, Is.EquivalentTo(new[] { new Cell(1, 1), new Cell(1, 2), new Cell(1, 3) }));
-		}
+		//	Assert.That(evolvedUniverse.LiveCells, Is.EquivalentTo(new[] { new Cell(1, 1), new Cell(1, 2), new Cell(1, 3) }));
+		//}
 	}
 }
